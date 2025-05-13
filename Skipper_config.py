64ae594273235cb6e5a7854f5de9f3fdd221900b
Darvesh Gorhe <darveshgorhe@gmail.com>
2025-05-13 15:16:39 -0400

    committing the RBFOX2 config for now since I'll use it for a while

--- Skipper_config.py
+++ Skipper_config.py
@@ -1,11 +1,7 @@
-# Configuration file 
-
-import os
-import sys
-import glob
+# Configuration file
 
 # Adjust REPO_PATH if necessary to make sure it contains the path to the pulled skipper repository when starting a skipper run
-REPO_PATH = "."
+REPO_PATH = "/burg/mjlab/users/dsg2157/skipper"
 
 ########################################
 # Customizable input files
@@ -14,15 +10,16 @@ MANIFEST = REPO_PATH + "/example/Example_manifest.csv"
 # Repeat table
 REPEAT_TABLE = REPO_PATH + "/annotations/repeatmasker.grch38.tsv.gz"
 # Genome fasta
-GENOME = REPO_PATH + "/annotations/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta"
+GENOME = "/burg/mjlab/projects/spidr-alignment-indices/GRCh38/GRCh38.primary_assembly.genome.fa"
 # STAR reference
-STAR_DIR = REPO_PATH + "/annotations/genome_ref"
+STAR_DIR = "/burg/mjlab/projects/spidr-alignment-indices/GRCh38/star"
 # Generated from STAR index
-CHROM_SIZES = REPO_PATH + "/annotations/genome_ref/chrNameLength.txt"
+CHROM_SIZES = STAR_DIR + "/chrNameLength.txt"
 # Use a GFF filtered for genes expressed in the cell type of interest
 GFF = REPO_PATH + "/annotations/gencode.v38.annotation.k562_totalrna.gt1.gff3.gz"
 # Customizable, with defaults
-BLACKLIST = REPO_PATH + "/annotations/encode3_eclip_blacklist.bed" # set to None for no blacklisting
+# set to None for no blacklisting
+BLACKLIST = REPO_PATH + "/annotations/encode3_eclip_blacklist.bed"
 GENE_SETS = REPO_PATH + "/annotations/c5.go.v7.5.1.symbols.gmt"
 GENE_SET_REFERENCE = REPO_PATH + "/annotations/encode3_go_terms.reference.tsv.gz"
 GENE_SET_DISTANCE = REPO_PATH + "/annotations/encode3_go_terms.jaccard_index.rds"
@@ -40,14 +37,16 @@ INFORMATIVE_READ = 1
 UNINFORMATIVE_READ = 3 - INFORMATIVE_READ
 # Use multiple input replicates to estimate overdispersion (preferred), or use multiple CLIP replicates
 # Skipper requires replicates to model the variance in read counts.
-OVERDISPERSION_MODE = "input" # input or clip
+OVERDISPERSION_MODE = "input"  # input or clip
 
 ########################################
 # Intermediate files and scripts for the Skipper run, user setup not required. Adjust the paths if necessary.
 
 # Skipper will partition the transcriptome and create feature annotations from the GFF
-PARTITION = REPO_PATH + "/annotations/gencode.v38.annotation.k562_totalrna.gt1.tiled_partition.bed.gz"
-FEATURE_ANNOTATIONS = REPO_PATH + "/annotations/gencode.v38.annotation.k562_totalrna.gt1.tiled_partition.features.tsv.gz"
+PARTITION = REPO_PATH + \
+    "/annotations/gencode.v38.annotation.k562_totalrna.gt1.tiled_partition.bed.gz"
+FEATURE_ANNOTATIONS = REPO_PATH + \
+    "/annotations/gencode.v38.annotation.k562_totalrna.gt1.tiled_partition.features.tsv.gz"
 # Skipper will sort the repeat table.
 REPEAT_BED = REPO_PATH + "/annotations/repeatmasker.grch38.sort.unique.bed.gz"
 # The directory contains Skipper scripts
